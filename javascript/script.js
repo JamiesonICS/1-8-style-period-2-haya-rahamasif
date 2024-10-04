19cfc9bba28843d53b35e0040484bed80a012a12
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2024-10-04 10:33:20 -0400

    final touches

--- javascript/script.js
+++ javascript/script.js
@@ -4,10 +4,13 @@ function showList() {
     document.getElementById("awards").style.display = "block";
     document.getElementById("see-awards").style.display = "none";
     document.getElementById("hide-awards").style.display = "block";
+    document.getElementById("scroll").style.display = "block";
   }
 
   function hideList() {
     document.getElementById("awards").style.display = "none";
     document.getElementById("see-awards").style.display = "block";
     document.getElementById("hide-awards").style.display = "none";
+    document.getElementById("scroll").style.display = "none";
+    
   }
