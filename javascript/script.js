9682237b25af79cfef8e3955b2e5fbe2e25e50fd
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2024-10-04 10:07:27 -0400

    fixed + styled buttons

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,13 @@
 
 
+function showList() {
+    document.getElementById("awards").style.display = "block";
+    document.getElementById("see-awards").style.display = "none";
+    document.getElementById("hide-awards").style.display = "block";
+  }
+
+  function hideList() {
+    document.getElementById("awards").style.display = "none";
+    document.getElementById("see-awards").style.display = "block";
+    document.getElementById("hide-awards").style.display = "none";
+  }
